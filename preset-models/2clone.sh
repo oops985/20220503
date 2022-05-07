dd3b8c1feed067075d558fe594ae10e6cd9cd9ff
oops985 <60307819+oops985@users.noreply.github.com>
2022-05-08 01:27:23 +0800

    Update 2clone.sh

--- preset-models/2clone.sh
+++ preset-models/2clone.sh
@@ -15,4 +15,4 @@ svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash
 git clone -b packages --single-branch https://github.com/xiaorouji/openwrt-passwall.git
 git clone -b luci --single-branch https://github.com/xiaorouji/openwrt-passwall.git luci-app-passwall
 git clone https://github.com/xiaorouji/openwrt-passwall2.git
-src-git helloworld https://github.com/fw876/helloworld
+git clone https://github.com/fw876/helloworld.git
