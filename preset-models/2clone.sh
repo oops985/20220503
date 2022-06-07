1482156c7e583ed2a17672ba342fab6f0c098573
oops985 <60307819+oops985@users.noreply.github.com>
2022-06-07 11:03:10 +0800

    Update 2clone.sh

--- preset-models/2clone.sh
+++ preset-models/2clone.sh
@@ -6,13 +6,13 @@ mv ./openwrt/* ./
 # 下载自定义插件
 mkdir -p package/myapps
 cd package/myapps
-git clone https://github.com/jerrykuku/luci-theme-argon.git
-git clone https://github.com/jerrykuku/luci-app-argon-config.git
-git clone https://github.com/jerrykuku/luci-app-vssr.git
+# git clone https://github.com/jerrykuku/luci-theme-argon.git
+# git clone https://github.com/jerrykuku/luci-app-argon-config.git
+# git clone https://github.com/jerrykuku/luci-app-vssr.git
 git clone https://github.com/jerrykuku/lua-maxminddb.git
 git clone https://github.com/Ausaci/luci-app-nat6-helper.git
-svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash
-git clone -b packages --single-branch https://github.com/xiaorouji/openwrt-passwall.git
-git clone -b luci --single-branch https://github.com/xiaorouji/openwrt-passwall.git luci-app-passwall
-git clone https://github.com/xiaorouji/openwrt-passwall2.git
-# git clone https://github.com/fw876/helloworld.git
+# svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash
+# git clone -b packages --single-branch https://github.com/xiaorouji/openwrt-passwall.git
+# git clone -b luci --single-branch https://github.com/xiaorouji/openwrt-passwall.git luci-app-passwall
+# git clone https://github.com/xiaorouji/openwrt-passwall2.git
+git clone https://github.com/fw876/helloworld.git
